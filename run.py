727a6be55b6abfd4d3c3511d4fcbeba2c6eb2677
David Sn <divad.nnamtdeis@gmail.com>
2020-03-23 15:54:08 +0100

    Increase thread count to 8 to avoid warnings
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from waitress import serve
 from pylife import app
 
 if __name__ == "__main__":
-    serve(app, host=app.config["HOST"], port=app.config["PORT"])
+    serve(app, host=app.config["HOST"], port=app.config["PORT"], threads=8)
