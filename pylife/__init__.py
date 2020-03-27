a82b999e2ec5af44086c7694d1f13aec9a09cb44
David Sn <divad.nnamtdeis@gmail.com>
2020-03-27 16:47:23 +0100

    Add flask-assets to bundle map css and js files
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- pylife/__init__.py
+++ pylife/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, send_from_directory
+from flask_assets import Environment, Bundle
 from flask_sqlalchemy import SQLAlchemy
 
 # Init app
 app = Flask(__name__, instance_relative_config=False)
 app.config.from_object("config")
+
+# Init web assets
+assets = Environment(app)
+assets.register("css_map", Bundle("css/leaflet.css", "css/map.css", filters="cssmin", output="css/map.bundle.css"))
+assets.register("js_map", Bundle("js/leaflet.js", "js/leaflet-rastercoords.js", "js/map.js", filters="jsmin", output="js/map.bundle.js"))
 
 # Load database
 db = SQLAlchemy(app)
